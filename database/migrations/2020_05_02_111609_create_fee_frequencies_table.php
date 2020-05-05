fd09bc8a4af2a96be103042f7c8892807627e4ce
Nitish <nbnitishbharti2@gmail.com>
2020-05-05 11:46:57 +0530

    Changes in fee frequency migration and seeder Changes in fee frequency migration and seeder

--- database/migrations/2020_05_02_111609_create_fee_frequencies_table.php
+++ database/migrations/2020_05_02_111609_create_fee_frequencies_table.php
@@ -16,6 +16,7 @@ class CreateFeeFrequenciesTable extends Migration
         Schema::create('fee_frequencies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name',255);
+            $table->integer('value');
             $table->timestamps();
         });
     }
--- database/seeds/FeeFrequencySeeder.php
+++ database/seeds/FeeFrequencySeeder.php
@@ -11,12 +11,31 @@ class FeeFrequencySeeder extends Seeder
      */
     public function run()
     {
-        $frequencies = ['Monthly','Quarterly','Half Yearly','Yearly'];
+        $frequencies = array(
+            0 => array(
+                'name'  => 'Monthly',
+                'value' => 12
+            ),
+            1 => array(
+                'name'  => 'Quarterly',
+                'value' => 4
+            ),
+            2 => array(
+                'name'  => 'Half Yearly',
+                'value' => 2
+            ),
+            3 => array(
+                'name'  => 'Yearly',
+                'value' => 1
+            ),
+        );
+        
         foreach ($frequencies as $key => $value) {
         	DB::table('fee_frequencies')->insert([
-	            'name' => $value,
-	            'created_at'=>date('Y-m-d H:i:s'),
-	            'updated_at'=>date('Y-m-d H:i:s'), 
+	            'name'         => $value['name'],
+                'value'        => $value['value'],
+	            'created_at'   => date('Y-m-d H:i:s'),
+	            'updated_at'   => date('Y-m-d H:i:s'), 
 	        ]);  
         }
     }
